842fc484a08faab32861ffd12087847220fbb691
Driver Station Laptop <controls@team3128.org>
2018-03-15 15:59:18 -0700

    Push for FORKLIFTPOS

--- src/board.java
+++ src/board.java
@@ -7,7 +7,6 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class board 
@@ -29,18 +28,22 @@ public class board
 		frame.getContentPane().add(AutoTitle);
 		frame.getContentPane().add(AutoDelay);
 		JFrame.setDefaultLookAndFeelDecorated(true);
-		JLabel cow = new JLabel("cCOWWWW");
-		cow.setFont(new Font("Arial", Font.BOLD, 24));
-		frame.getContentPane().add(cow);
+		// dont change this. dont use addcomponent, i need to be able to change the stuffs.
+		JLabel forkpos = new JLabel("Forklift Position: ");
+		JTextField forkposValue = new JTextField("0.0", 10);
+		forkpos.setFont(new Font("Arial", Font.BOLD, 24));
+		forkposValue.setFont(new Font("Arial", Font.BOLD, 24));
+		frame.getContentPane().add(forkpos);
+		frame.getContentPane().add(forkposValue);
 		frame.setSize(1600, 900);
-		//frame.pack();
+		//frame.pack();. Don't pack, ruins sizing.
 		frame.setVisible(true);
 		NetworkTableInstance inst = NetworkTableInstance.getDefault();
 		NetworkTable table = inst.getTable("datatable");
-		NetworkTableEntry xEntry = table.getEntry("x");
-		NetworkTableEntry yEntry = table.getEntry("y");
-		NetworkTableEntry timerEntry = table.getEntry("Timer");
-		inst.startClientTeam(3128); // where TEAM=190, 294, etc, or use inst.startClient("hostname") or similar
+		//NetworkTableEntry xEntry = table.getEntry("x");
+		//NetworkTableEntry yEntry = table.getEntry("y");
+		//NetworkTableEntry timerEntry = table.getEntry("Timer");
+		inst.startClient("10.31.28.2"); // where TEAM=190, 294, etc, or use inst.startClient("hostname") or similar
 		inst.startDSClient(); // recommended if running on DS computer; this
 								// gets the robot IP from the DS
 		while (true)
@@ -48,17 +51,17 @@ public class board
 			try
 			{
 				Thread.sleep(1000);
-				cow.setText(table.getEntry("cow").getValue().getString());
+				forkposValue.setText(String.valueOf(table.getEntry("forkliftPosition").getValue().getDouble()));
 			}
 			catch (InterruptedException ex)
 			{
-				System.out.println("interrupted");
+				System.out.println("NarwhalDashboard interruption error.");
 				return;
 			}
-			double x = xEntry.getDouble(0.0);
-			double y = yEntry.getDouble(0.0);
-			double MatchTimer = timerEntry.getDouble(0.0);
-			System.out.println("X: " + x + " Y: " + y);
+			//double x = xEntry.getDouble(0.0);
+			//double y = yEntry.getDouble(0.0);
+			//double MatchTimer = timerEntry.getDouble(0.0);
+			//System.out.println("X: " + x + " Y: " + y);
 		}
 	}
 	public void addComponent(String key, String value) {
