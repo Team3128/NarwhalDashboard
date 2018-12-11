feed0a5ffb0c9c8660381128ad6a9c1ae55fdd05
Team 3128: Controls <controls@team3128.org>
2018-12-10 17:11:03 -0800

    Bugfixes

--- guido/functions.js
+++ guido/functions.js
@@ -1,5 +1,8 @@
+var speedGauge;
+var forkBar;
+
 function initialize() {
-    var speedGauge = makeGauge('speed_gauge', 200, 0, 20, [0,4,8,12,16,20], 4, [
+    speedGauge = makeGauge('speed_gauge', 200, 0, 20, [0,4,8,12,16,20], 4, [
             {from:  0, to:  8, color: "#d60000"},
             {from:  8, to: 15, color: "#efdb00"},
             {from: 15, to: 20, color: "#41c603"}
@@ -7,7 +10,7 @@ function initialize() {
     speedGauge.value = 0;
     speedGauge.draw();
 
-    var forkBar = makeBar('fork_bar', 200, 500, 0, 8, [0,1,2,3,4,5,6,7,8], 4, "both", [
+    forkBar = makeBar('fork_bar', 200, 500, 0, 8, [0,1,2,3,4,5,6,7,8], 4, "both", [
             {from: 0, to: 2, color: "#d60000"},
             {from: 2, to: 4, color: "#efdb00"},
             {from: 4, to: 5, color: "#41c603"},
