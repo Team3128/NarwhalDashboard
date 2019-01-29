0020946cc2d16af7e784abade7391b562ef03e1f
crcali <33082459+crcali@users.noreply.github.com>
2019-01-29 15:29:49 -0800

    PID Dashboard

--- prebot/functions.js
+++ prebot/functions.js
@@ -3,5 +3,15 @@ function initialize() {
 }
 
 function refresh(json) {
+    {% if page.tab == 'Drive Calibration Utility' %}
+        document.getElementById('wheelCirc').innerText = json['wheelCirc'] + 'in';
+        document.getElementById('leftKf').innerText = json['leftKf'];
+        document.getElementById('rightKf').innerText = json['rightKf'] + 'in';
+        document.getElementById('leftSpeedScalar').innerText = json['leftSpeedScalar'] + 'in';
+        document.getElementById('rightSpeedScalar').innerText = json['rightSpeedScalar'] + 'in';
+        document.getElementById('wheelBase').innerText = json['wheelBase'] + 'in';
+        document.getElementById('leftVelocityError').innerText = json['leftVelocityError'] + 'in';
+        document.getElementById('rightVelocityError').innerText = json['rightVelocityError'] + 'in';
 
+    {% endif %}
 }
