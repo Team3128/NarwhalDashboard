53c9ea58ad52e4f757cc52c55084c013b611bb08
Ronak Roy <ronak.r123@gmail.com>
2019-01-29 17:34:59 -0800

    Tweaked value display

--- prebot/functions.js
+++ prebot/functions.js
@@ -6,12 +6,11 @@ function refresh(json) {
     {% if page.tab == 'Drive Calibration Utility' %}
         document.getElementById('wheelCirc').innerText = json['wheelCirc'] + 'in';
         document.getElementById('leftKf').innerText = json['leftKf'];
-        document.getElementById('rightKf').innerText = json['rightKf'] + 'in';
-        document.getElementById('leftSpeedScalar').innerText = json['leftSpeedScalar'] + 'in';
-        document.getElementById('rightSpeedScalar').innerText = json['rightSpeedScalar'] + 'in';
+        document.getElementById('rightKf').innerText = json['rightKf'];
+        document.getElementById('leftSpeedScalar').innerText = json['leftSpeedScalar'];
+        document.getElementById('rightSpeedScalar').innerText = json['rightSpeedScalar'];
         document.getElementById('wheelBase').innerText = json['wheelBase'] + 'in';
-        document.getElementById('leftVelocityError').innerText = json['leftVelocityError'] + 'in';
-        document.getElementById('rightVelocityError').innerText = json['rightVelocityError'] + 'in';
-
+        document.getElementById('leftVelocityError').innerText = json['leftVelocityError'] + 'nu/100ms';
+        document.getElementById('rightVelocityError').innerText = json['rightVelocityError'] + 'nu/100ms';
     {% endif %}
 }
