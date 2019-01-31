157f0d37e099d352c048fb864fcfd99af1e3fd86
Team 3128: Controls <controls@team3128.org>
2019-01-30 19:20:28 -0800

    rounding values

--- prebot/functions.js
+++ prebot/functions.js
@@ -3,11 +3,14 @@ function initialize() {
 }
 
 function refresh(json) {
-    document.getElementById('wheelCirc').innerText = json['wheelCirc'];
-    document.getElementById('leftKf').innerText = json['leftKf'];
-    document.getElementById('rightKf').innerText = json['rightKf'];
-    document.getElementById('leftSpeedScalar').innerText = json['leftSpeedScalar'];
-    document.getElementById('rightSpeedScalar').innerText = json['rightSpeedScalar'];
+    document.getElementById('wheelCirc').innerText = Number(json['wheelCirc']).toFixed(2);
+
+    document.getElementById('leftKf').innerText = Number(json['leftKf']).toFixed(4);
+    document.getElementById('rightKf').innerText = Number(json['rightKf']).toFixed(4);
+
+    document.getElementById('leftSpeedScalar').innerText =  Number(json['leftSpeedScalar']).toFixed(4);
+    document.getElementById('rightSpeedScalar').innerText =  Number(json['rightSpeedScalar']).toFixed(4);
+
     document.getElementById('wheelBase').innerText = json['wheelBase'];
     document.getElementById('leftVelocityError').innerText = json['leftVelocityError'];
     document.getElementById('rightVelocityError').innerText = json['rightVelocityError'];
