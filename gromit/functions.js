545b90888059704a9ee9c8711a0fa9a92e2b423d
Team 3128: Controls <controls@team3128.org>
2019-02-18 09:50:08 -0800

    ronak

--- gromit/functions.js
+++ gromit/functions.js
@@ -1,10 +1,17 @@
 var scoring_target, game_element;
+var gear;
+
+var gear_indicator;
 
 function initialize() {
     initDCU();
 
-    scoring_height = 'mid';
+    scoring_target = 'mid';
     game_element = 'hatch_panel';
+
+    gear = 'false';
+
+    gear_indicator = getElement('gear_indicator');
 }
 
 function refresh(json) {
@@ -28,6 +35,23 @@ function refresh(json) {
 
         getElement(game_element + '_indicator').classList.remove('grey');
         getElement(game_element + '_indicator').classList.add('green');
+    }
+
+    if (json['gear'] != gear) {
+        gear = json['gear'];
+
+        if (gear == 'false') {
+            gear_indicator.classList.remove('green');
+            gear_indicator.classList.add('orange');
+
+            gear_indicator.innerHTML = "Low<br>Gear";
+        }
+        else {
+            gear_indicator.classList.add('green');
+            gear_indicator.classList.remove('orange');
+
+            gear_indicator.innerHTML = "High<br>Gear";
+        }
     }
     
 }
