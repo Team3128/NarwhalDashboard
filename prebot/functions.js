b04adafbaa2de98b5f5363a2a86c2dd16bef5b8c
tyco333 <34726618+tyco333@users.noreply.github.com>
2020-01-25 11:15:05 -0800

    everythings changed
    e

--- prebot/functions.js
+++ prebot/functions.js
@@ -1,7 +1,19 @@
 function initialize() {
-    initDCU();
+    //initDCU(); 
+    ErrorCatcher = 'true';
+
+    ErrorCatcher = getElement('ErrorValue');
 }
 
 function refresh(json) {
-    updateDCU();
+   // updateDCU();
+
+    if(ErrorCatcher.innerHTML == "undefined"){
+        ErrorCatcher.innerHTML = 'ErrorCatcher has not been run yet'
+    }
+    if (json['ErrorCatcher'] != ErrorCatcher.innerHTML) {
+        
+        ErrorCatcher.innerHTML = json['ErrorCatcher'];
+
+    }
 }
