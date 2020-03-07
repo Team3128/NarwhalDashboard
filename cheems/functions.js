89d238ebab842b1f01709c3bfd9e17b9ad8ec39b
jude-lif <43428384+Jugue123@users.noreply.github.com>
2020-03-07 07:56:18 -0800

    yes

--- cheems/functions.js
+++ cheems/functions.js
@@ -4,6 +4,10 @@ function initialize() {
     ball_count = getElement('ball_count');
     hopper_indicator = getElement('hopper_indicator');
 
+    shooting_state = getElement('shooting_state');
+    SetStateLong = 'true';
+    SetStateMid = 'true';
+    SetStateShort = 'true';
 
     ErrorCatcherCAN = 'true';
     ErrorCatcherCAN = getElement('ErrorValueCAN');
@@ -33,6 +37,43 @@ function refresh(json) {
         hopper_indicator.src = "/cheems/assets/3_ball.png";
     } else {
         hopper_indicator.src = "/cheems/assets/error_ball.png";
+    }
+
+    if (json['SetStateLong'] != SetStateLong.innerHTML) {
+        getElement(SetStateLong).classList.remove('green');
+        getElement(SetStateLong).classList.add('grey');
+        
+        SetStateLong.innerHTML = json['SetStateLong'];
+
+        getElement(SetStateLong).classList.remove('grey');
+        getElement(SetStateLong).classList.add('green');
+    }
+    if (json['SetStateMid'] != SetStateMid.innerHTML) {
+        getElement(SetStateMid).classList.remove('green');
+        getElement(SetStateMid).classList.add('grey');
+        
+        SetStateMid.innerHTML = json['SetStateMid'];
+
+        getElement(SetStateMid).classList.remove('grey');
+        getElement(SetStateMid).classList.add('green');
+    }
+    if (json['SetStateShort'] != SetStateShort.innerHTML) {
+        getElement(SetStateShort).classList.remove('green');
+        getElement(SetStateShort).classList.add('grey');
+        
+        SetStateShort.innerHTML = json['SetStateShort'];
+
+        getElement(SetStateShort).classList.remove('grey');
+        getElement(SetStateShort).classList.add('green');
+    }
+    if (json['shooting_state'] != shooting_state){
+        getElement('SetState'+shooting_state).classList.remove('green');
+        getElement('SetState'+shooting_state).classList.add('grey');
+
+        shooting_state = json['shooting_state'];
+
+        getElement('SetState'+shooting_state).classList.remove('grey');
+        getElement('SetState'+shooting_state).classList.add('green');
     }
 
     if (json['ErrorCatcherCAN'] != ErrorCatcherCAN.innerHTML) {
