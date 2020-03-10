c9bb3a85dfc24a889dd6511cd39843e1787a84e6
Mason Holst <mvhsan@ccom.ucsd.edu>
2020-03-09 17:30:48 -0700

    vision/manual align toggle

--- cheems/functions.js
+++ cheems/functions.js
@@ -49,6 +49,18 @@ function refresh(json) {
         getElement('SetState'+shooting_state).classList.add('green');
     }
 
+    if(json['align_toggle'] == 'true'){
+        getElement('VisionAlign').classList.remove('green');
+        getElement('VisionAlign').classList.add('grey');
+        getElement('ManualAlign').classList.remove('grey');
+        getElement('ManualAlign').classList.add('green');
+    } else {
+        getElement('ManualAlign').classList.remove('green');
+        getElement('ManualAlign').classList.add('grey');
+        getElement('VisionAlign').classList.remove('grey');
+        getElement('VisionAlign').classList.add('green'); 
+    }
+
     if (json['ErrorCatcherCAN'] != ErrorCatcherCAN.innerHTML) {
         
         ErrorCatcherCAN.innerHTML = json['ErrorCatcherCAN'];
